648590f5624b90e31ce311b23db49b881b97c23e
Johan Back <johan.backs@gmail.com>
2020-06-13 23:41:38 -0300

    add nunjunks

--- src/server.js
+++ src/server.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const server = express();
+const nunjucks = require("nunjucks");
+
+//conf nunjucks
+nunjucks.configure("src/views", {
+  express:server,
+  noCache: true
+})
+
+
+server.use(express.static("public"))
 
 server.get("/" , (req, res ) => {
-  res.send("Ola Dev")
+  res.sendFile(__dirname + "/views/index.html")
 })
+server.get("/create-point" , (req, res ) => {
+  res.sendFile(__dirname + "/views/create-point.html")
+})
+server.get("/search-results" , (req, res ) => {
+  res.sendFile(__dirname + "/views/search-results.html")
+})
+
+
 server.listen(3000)
